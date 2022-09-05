38dc7f7a85304f5ab1c340ff7428ff452749df2b
mustafaDabah <mustafadabah5555@gmail.com>
2022-09-05 19:15:47 +0200

    first commit

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+    <Head>
+        <title>About</title>
+    </Head>
+    <div>About</div>
+    </div>
+  )
+}
+
+export default About
---
+++ pages/article/[id]/index.js
@@ -0,0 +1,45 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
+const Article = ({ article }) => {
+  // const router = useRouter()
+  // const { id } = router.query
+
+  return (
+    <div>
+      {/* <Meta title={article.title} description={article.excerpt} /> */}
+      <h1>{article.title}</h1>
+      <p>{article.body}</p>
+      <br />
+      <Link href='/'>Go Back</Link>
+    </div>
+  )
+}
+
+export const getStaticProps = async (context) => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+
+  const article = await res.json()
+
+  return {
+    props: {
+      article,
+    },
+  }
+}
+
+export const getStaticPaths = async (context) => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+
+  const articles = await res.json()
+
+  const ids = articles?.map((article) => article.id)
+  const paths = ids?.map((id) => ({ params: { id: id.toString() }}))
+
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export default Article
